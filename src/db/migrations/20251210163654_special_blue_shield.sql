106faa9b69b9afca3d71a707ad0e7ea02485c690
Ethan Saso <ethansaso@gmail.com>
2025-12-10 08:37:23 -0800

    Addition of subgenus/section/complex to rank enum

---
+++ src/db/migrations/20251210163654_special_blue_shield.sql
@@ -0,0 +1,3 @@
+ALTER TYPE "public"."taxon_rank" ADD VALUE 'subgenus' BEFORE 'species';--> statement-breakpoint
+ALTER TYPE "public"."taxon_rank" ADD VALUE 'section' BEFORE 'species';--> statement-breakpoint
+ALTER TYPE "public"."taxon_rank" ADD VALUE 'complex' BEFORE 'species';
