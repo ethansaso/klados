f490bd8ee5cd5fba432eae7de3bc3f718c28aab2
Ethan Saso <ethansaso@gmail.com>
2026-01-20 19:23:23 -0800

    Add descriptions to group/character in cards; generalize information css

--- src/routes/_app/taxa/$id/-characters/GroupCard.tsx
+++ src/routes/_app/taxa/$id/-characters/GroupCard.tsx
@@ -1,21 +1,39 @@
-import { Card, DataList, Heading, Separator } from "@radix-ui/themes";
+import { Card, DataList, Heading, Separator, Tooltip } from "@radix-ui/themes";
 import { CharacterStateDisplay } from "../../../../../components/trait-tokens/CharacterStateDisplay";
 import { GroupedCharacterStates } from "../../../../../lib/domain/character-states/utils";
 
 export const GroupCard = ({ group }: { group: GroupedCharacterStates }) => {
+  const cardHeaderComponent = group.groupDescription ? (
+    <Tooltip content={group.groupDescription}>
+      <span className="has-information">{group.groupLabel}</span>
+    </Tooltip>
+  ) : (
+    group.groupLabel
+  );
+
   return (
     <Card size="2">
-      <Heading size="2">{group.groupLabel}</Heading>
+      <Heading size="2">{cardHeaderComponent}</Heading>
       <Separator size="4" mt="1" mb="3" />
       <DataList.Root size="2">
-        {group.states.map((state) => (
-          <DataList.Item key={state.characterId}>
-            <DataList.Label>{state.characterLabel}</DataList.Label>
-            <DataList.Value>
-              <CharacterStateDisplay state={state} />
-            </DataList.Value>
-          </DataList.Item>
-        ))}
+        {group.states.map((state) => {
+          const dlLabel = state.characterDescription ? (
+            <Tooltip content={state.characterDescription}>
+              <span className="has-information">{state.characterLabel}</span>
+            </Tooltip>
+          ) : (
+            state.characterLabel
+          );
+
+          return (
+            <DataList.Item key={state.characterId}>
+              <DataList.Label>{dlLabel}</DataList.Label>
+              <DataList.Value>
+                <CharacterStateDisplay state={state} />
+              </DataList.Value>
+            </DataList.Item>
+          );
+        })}
       </DataList.Root>
     </Card>
   );
