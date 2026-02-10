6521be6561f8fd98acd34f65a11430384d35f6ac
Ethan Saso <ethansaso@gmail.com>
2026-02-10 12:54:49 -0800

    Remove bad import to nonexistent override css

--- src/routes/_app/guides/route.tsx
+++ src/routes/_app/guides/route.tsx
@@ -1,5 +1,4 @@
 import "../../../assets/styles/react-flow/nodes.css";
-import "../../../assets/styles/react-flow/override.css";
 
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 
