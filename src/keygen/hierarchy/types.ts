afc1ed86355eac7690cd1413e7ec71e7c1eecf5b
Ethan Saso <ethansaso@gmail.com>
2026-08-23 11:57:09 -0700

    properly handle 'absent' in keygen

--- src/keygen/hierarchy/types.ts
+++ src/keygen/hierarchy/types.ts
@@ -2,5 +2,9 @@ import type { FeatureStateDTO } from "../../lib/domain/states/types";
 import type { TaxonHierarchyDTO } from "../../lib/domain/taxa/types";
 
 export type HierarchyTaxonNode = TaxonHierarchyDTO & {
+  /** Features the taxon bears. Excludes 'absent' feature states. */
   states: FeatureStateDTO[];
+
+  /** All features the taxon has assigned, including 'absent' states. */
+  allStates: FeatureStateDTO[];
 };
