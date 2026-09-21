7b5ecf03070254c313d6000f894f368e24198af6
Ethan Saso <ethansaso@gmail.com>
2026-09-20 18:14:53 -0700

    fix padding

--- src/routes/_app/taxa/$id/edit/-characters/search/CharacterStateSearch.tsx
+++ src/routes/_app/taxa/$id/edit/-characters/search/CharacterStateSearch.tsx
@@ -1,7 +1,6 @@
 import { Badge, Box, Button, Flex, Link, Text } from "@radix-ui/themes";
 import { useServerFn } from "@tanstack/react-start";
 import { useCallback, useId, useRef, useState } from "react";
-import { PiPencil } from "react-icons/pi";
 import { InputCombobox } from "../../../../../../../components/inputs/combobox/InputCombobox";
 import type { TraitSuggestion } from "../../../../../../../lib/domain/suggestions/types";
 import { listCharacterStateSuggestionsFn } from "../../../../../../../lib/server-fns/suggestions/listCharacterStateSuggestionsFn";
@@ -152,14 +151,13 @@ export function CharacterStateSearch({
               </InputCombobox.Item>
             ))}
           </InputCombobox.List>
-          <Box m="-2" p="2" mt="0" className="character-search__feature-footer">
+          <Box m="-2" p="2" mt="2" className="character-search__feature-footer">
             <Button asChild size="1" variant="ghost">
               <Link
                 href={`/glossary/features/${featureId}`}
                 target="_blank"
                 rel="noreferrer"
               >
-                <PiPencil size={12} />
                 <Text as="p">Edit feature in glossary</Text>
               </Link>
             </Button>
